40a7e4f08b1075de2ae374f461adfbf395bf5840
Good Friend Job <somrosomro911@gmail.com>
2024-12-17 21:28:51 +0900

    init

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
+
 const dotenv = require("dotenv");
 const session = require("express-session");
 const morgan = require("morgan");
@@ -28,10 +30,11 @@ const PORT = process.env.PORT || 5000;
 
 // Database connection
 db()
-  .then(() => logger.info("Connected to MongoDB"))
-  .catch((err) => logger.error("MongoDB connection error:", err));
+.then(() => logger.info("Connected to MongoDB"))
+.catch((err) => logger.error("MongoDB connection error:", err));
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev")); // Logging HTTP requests
